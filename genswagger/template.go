6fa67e7cc8479c7a289a7d0a8ca921000b9875fd
vincent <vincent.xu@saisiyun.com>
2020-01-31 16:31:17 +0800

    Enum

--- genswagger/template.go
+++ genswagger/template.go
@@ -392,7 +392,7 @@ func schemaOfField(f *descriptor.Field, reg *descriptor.Registry, refs refMap) s
 	var props *swaggerSchemaObjectProperties
 
 	switch ft := fd.GetType(); ft {
-	case pbdescriptor.FieldDescriptorProto_TYPE_ENUM, pbdescriptor.FieldDescriptorProto_TYPE_MESSAGE, pbdescriptor.FieldDescriptorProto_TYPE_GROUP:
+	case pbdescriptor.FieldDescriptorProto_TYPE_MESSAGE, pbdescriptor.FieldDescriptorProto_TYPE_GROUP:
 		if wktSchema, ok := wktSchemas[fd.GetTypeName()]; ok {
 			core = wktSchema
 
@@ -694,17 +694,17 @@ func renderServices(services []*descriptor.Service, paths swaggerPathsObject, re
 						} else {
 							return fmt.Errorf("only primitive and well-known types are allowed in path parameters")
 						}
-					//case pbdescriptor.FieldDescriptorProto_TYPE_ENUM:
-					//	paramType = "string"
-					//	paramFormat = ""
-					//	enum, err := reg.LookupEnum("", parameter.Target.GetTypeName())
-					//	if err != nil {
-					//		return err
-					//	}
-					//	enumNames = listEnumNames(enum)
-					//	schema := schemaOfField(parameter.Target, reg, customRefs)
-					//	desc = schema.Description
-					//	defaultValue = schema.Default
+					case pbdescriptor.FieldDescriptorProto_TYPE_ENUM:
+						paramType = "string"
+						paramFormat = ""
+						enum, err := reg.LookupEnum("", parameter.Target.GetTypeName())
+						if err != nil {
+							return err
+						}
+						enumNames = listEnumNames(enum)
+						schema := schemaOfField(parameter.Target, reg, customRefs)
+						desc = schema.Description
+						defaultValue = schema.Default
 					default:
 						var ok bool
 						paramType, paramFormat, ok = primitiveSchema(pt)
